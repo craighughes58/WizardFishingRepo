5e2f165bd17e7c98712b481b5d47638898e114c9
MrDoctorb <zane@indybrowns.org>
2023-12-16 16:52:02 -0500

    Some very basic player movement

--- Wizard Fishing/Assets/Scripts/Player/PlayerMovementController.cs
+++ Wizard Fishing/Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D rb;
+    [SerializeField] float moveForce, maxSpeed, decendSpeed, rotSpeed, maxRotSpeed;
+    float moveDir, rotDir;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.AddForceAtPosition(Vector2.right * moveDir, Vector2.up * .15f);
+        rb.AddTorque(rotSpeed * rotDir);
+
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), -decendSpeed);
+        rb.angularVelocity = Mathf.Clamp(rb.angularVelocity, -maxRotSpeed, maxRotSpeed);
+       
+    }
+
+    void OnMove(InputValue input)
+    {
+        moveDir = input.Get<float>();
+    }
+
+    void OnRotate(InputValue input)
+    {
+        rotDir = input.Get<float>();
     }
 }
