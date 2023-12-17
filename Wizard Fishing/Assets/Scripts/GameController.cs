b41a9a3f21645ad87b096b0909e3118ca68af76a
MrDoctorb <zane@indybrowns.org>
2023-12-17 14:18:25 -0500

    Game Controller and TMP

---
+++ Wizard Fishing/Assets/Scripts/GameController.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public enum FishType
+{
+    Type0 = 0,
+    Type1 = 1,
+    Type2 = 2,
+    Type3 = 3
+}
+
+
+public class GameController : MonoBehaviour
+{
+    static GameController instance;
+
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] int startingTime, startingTimeTillBigOne;
+    Coroutine fishingTimer;
+    float timeRemaining, timeTillBigOne;
+    int[] fishCollected = new int[] { 0, 0, 0, 0 };
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        StartFishing();
+    }
+
+    /// <summary>
+    /// Start the timer and fish!
+    /// </summary>
+    public void StartFishing()
+    {
+        timeRemaining = startingTime;
+        timeTillBigOne  = startingTimeTillBigOne;
+        fishingTimer = StartCoroutine(FishingTimer());
+    }
+
+    /// <summary>
+    /// Stop fishing
+    /// Convert collected fish to spells
+    /// </summary>
+    public void EndFishing()
+    {
+        print("Game is over!!");
+    }
+
+    IEnumerator FishingTimer()
+    {
+        while (timeRemaining > 0)
+        {
+            yield return null;
+            timeRemaining -= Time.deltaTime;
+            timerText.text = ((int)timeRemaining).ToString();
+
+            if (timeTillBigOne <= startingTimeTillBigOne)
+            {
+                if (timeTillBigOne < 0)
+                {
+                    SpawnBigOne();
+                    timeTillBigOne = startingTimeTillBigOne + 1;
+                    continue;
+                }
+                timeTillBigOne -= Time.deltaTime;
+            }
+
+        }
+        EndFishing();
+    }
+
+    public float GetTimeRemaining()
+    {
+        return timeRemaining;
+    }
+
+    public void AddTime(float amount)
+    {
+        timeRemaining += amount;
+        if (timeRemaining <= 0) EndFishing();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="fishType">What number means which fish is still up in the air</param>
+    public void CollectFish(FishType fishType)
+    {
+        if ((int)fishType >= fishCollected.Length)
+        {
+            Debug.LogWarning("HEY!!! THE FISH COLLECTED ARRAY ISN'T BIG ENOUGH FOR THE FISH OF TYPE " +  fishType);
+            return;
+        }
+
+        fishCollected[(int)fishType]++;
+    }
+
+    void SpawnBigOne()
+    {
+        print("Spawning The Big One");
+    }
+}
