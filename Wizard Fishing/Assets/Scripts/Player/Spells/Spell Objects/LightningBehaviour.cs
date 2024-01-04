002716789a9d74d77762d8cbc9c0d4778029d3f1
craighughes58 <90798245+craighughes58@users.noreply.github.com>
2024-01-04 16:33:39 -0600

    Update LightningBehaviour.cs

--- Wizard Fishing/Assets/Scripts/Player/Spells/Spell Objects/LightningBehaviour.cs
+++ Wizard Fishing/Assets/Scripts/Player/Spells/Spell Objects/LightningBehaviour.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class LightningBehaviour : MonoBehaviour
 {
+
+    #region Private Variables
+
+    #endregion
+
+    #region Serialized Variables
+
+
+    [Tooltip("")]
+    [SerializeField] private int _damage;
+
+    [Tooltip("")]
+    [SerializeField] private float _speed;
+
+    [Tooltip("")]
+    [SerializeField] private float _radius;
+
+
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    #region Targeting
+    
+    #endregion
 }
